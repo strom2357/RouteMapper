c5c88f56bb0e1a91c075dc7834b4c801b718e628
Erik Strom <strom2357@gmail.com>
2014-12-05 09:08:34 -0800

    Phase 5: Remove 'hello from backbone' alert

--- app/assets/javascripts/route_mapper.js
+++ app/assets/javascripts/route_mapper.js
@@ -4,7 +4,6 @@ window.RouteMapper = {
   Views: {},
   Routers: {},
   initialize: function() {
-    alert('Hello from Backbone!');
     new RouteMapper.Routers.Router({
   		$rootEl: $("#content")
   	});
