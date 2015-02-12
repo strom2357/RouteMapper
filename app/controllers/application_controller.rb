01c1a066e69394fd7b62c00fe407914d8b3b1428
Erik Strom <strom2357@gmail.com>
2015-02-12 13:06:00 -0800

    require_signed_in goes to new user, not new session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,6 +25,6 @@ class ApplicationController < ActionController::Base
   end
 
   def require_signed_in!
-  	redirect_to new_session_url unless signed_in?
+  	redirect_to new_user_url unless signed_in?
   end
 end
